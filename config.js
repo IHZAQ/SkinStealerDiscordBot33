d67aaaedccf1a90fe8969be5dd66d680429cc05d
Haziq Zamri 3 <81473609+IHZAQSTORM33@users.noreply.github.com>
2023-03-31 22:07:27 +0800

    Format

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ export default {
 	},
 	norme: {
 		footer: "Lightning Craft © 2023"
-	}, 
+	},
 	developers: [
-    process.env.FOUNDER_ID
-  ], 
-  channels: {
-    guildjoin: "", //Join Server Logs Channel Id
-    guildleft: "", //Left Server Logs Channel Id
-    reportlogs: "" //Report Bugs Logs Channel Id
-  }, 
-  guild_id: process.env.GUILD_ID
+		process.env.FOUNDER_ID
+	],
+	channels: {
+		guildjoin: "", //Join Server Logs Channel Id
+		guildleft: "", //Left Server Logs Channel Id
+		reportlogs: "" //Report Bugs Logs Channel Id
+	},
+	guild_id: process.env.GUILD_ID
 }
