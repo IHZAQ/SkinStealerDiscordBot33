743c9c74ab4bd0d59f12158b1037258ca0668663
IHZAQSTORM <IHZAQSTORM>
2023-04-15 14:52:07 +0800

    Removed GuildCreate and GuildDelete events

--- config.js
+++ config.js
@@ -10,8 +10,6 @@ export default {
 		process.env.FOUNDER_ID
 	],
 	channels: {
-		guildjoin: "", //Join Server Logs Channel Id
-		guildleft: "", //Left Server Logs Channel Id
 		reportlogs: "" //Report Bugs Logs Channel Id
 	},
 	guild_id: process.env.GUILD_ID
