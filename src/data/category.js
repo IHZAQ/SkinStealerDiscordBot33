ed398ba058560d7389bf0d7083650ea94bd86a0a
IHZAQSTORM33 <81473609+IHZAQSTORM33@users.noreply.github.com>
2024-02-17 02:15:06 +0800

    Small change to help commands

--- src/data/category.js
+++ src/data/category.js
@@ -2,12 +2,14 @@ export const emoji = new Map([
   ["Minecraft Utilities", "<:minecraft:1082646921103032340>"],
   ["Roblox Tools", "<:roblox:1082647296208019557>"],
   ["Xbox Live", "<:xbox:1083322489276465172>"],
-  ["General", "⚙️"]
+  ["General", "⚙️"],
+  ["Statistics", "📊"]
 ])
 
 export const description = new Map([
   ["Minecraft Utilities", "All Minecraft Related Commands"],
   ["Roblox Tools", "All Useful Roblox Tools Commands"],
   ["Xbox Live", "All Useful Xbox Live Commands"],
-  ["General", "All Basic Bot Commands"]
+  ["General", "All Basic Bot Commands"],
+  ["Statistics", "Stats of Games"]
 ])
